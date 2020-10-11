0e5ec11ef1c4edcf54934e2c08557f934e182356
siberian122 <siberian1000@gmail.com>
2020-10-11 22:12:03 +0900

    commit

--- HHKB/HHKB-C.py
+++ HHKB/HHKB-C.py
@@ -2,10 +2,16 @@ def main():
     n=int(input())
     p=list(map(int,input().split()))
     l=[0 for _ in range(max(p)+2)]
-    #print(l)
-    reserc=l.index
+    cnt=0
     for i in p:
         l[i]=1
-        print(reserc(0))
+        if l[cnt]==0:
+            print(cnt)
+        else:
+            while True:
+                cnt+=1
+                if l[cnt]==0:
+                    break
+            print(cnt)
 
 main()
