85c0cc7f5c9f1601415f118fda17b3671dba325c
siberian122 <siberian1000@gmail.com>
2020-10-13 13:09:09 +0900

    Update Wanna-go-back-home.py

--- practice/Wanna-go-back-home.py
+++ practice/Wanna-go-back-home.py
@@ -9,3 +9,13 @@ for i in s:
         count[2]+=1
     elif i=='W':
         count[3]+=1
+if count[0]>0 and count[1]==0:
+    print('No')
+elif count[1]>0 and count[0]==0:
+    print('No')
+elif count[2]>0 and count[3]==0:
+    print('No')
+elif count[3]>0 and count[2]==0:
+    print('No')
+else:
+    print('Yes')
