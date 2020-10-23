c0dacfcad094f436b6653828e7a2a17a4929c801
siberian122 <siberian1000@gmail.com>
2020-10-24 00:32:17 +0900

    Update Minesweeper.py

--- practice/Minesweeper.py
+++ practice/Minesweeper.py
@@ -5,7 +5,7 @@ for i in range(h):
         ans=0
         for k in range(i-1,i+2):
             for l in range(j-1,j+2):
-                if min(k,l)>0 and k<h and l<w and s[k][l]=='#':
+                if min(k,l)>=0 and k<h and l<w and s[k][l]=='#':
                     ans+=1
         if s[i][j]=='.':
             s[i][j]=ans
