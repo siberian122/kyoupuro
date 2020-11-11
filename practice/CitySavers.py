ea8011cb5a12cecf5f9483fe7541d70ac791b796
siberian122 <siberian1000@gmail.com>
2020-11-11 23:24:45 +0900

    Update CitySavers.py

--- practice/CitySavers.py
+++ practice/CitySavers.py
@@ -1,10 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-ans=[]
-ans.append(min(a[0],b[0]))
-for i in range(1,n):
-    ans.append(min(a[i+1],b[i]-ans[i-1]))
-
-
+ans=0
+for i in range(n):
+    if a[i]<=b[i]:
+        ans+=a[i]
+        b[i]-=a[i]
+        ans+=min(a[i+1],b[i])
+        a[i+1]=max(0,a[i+1]-b[i])
+    else:
+        ans+=b[i]
+print(ans)
     
