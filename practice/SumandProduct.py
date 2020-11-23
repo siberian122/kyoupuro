ae96ea04d2c0c5d0e1c4a07ac3932c70c6b4934f
siberian122 <siberian1000@gmail.com>
2020-11-23 23:27:47 +0900

    Update SumandProduct.py

--- practice/SumandProduct.py
+++ practice/SumandProduct.py
@@ -1,26 +1,7 @@
 s, p = map(int, input().split())
-
-
-def make_divisors(n):
-    lower_divisors, upper_divisors = [], []
-    i = 1
-    while i*i <= n:
-        if n % i == 0:
-            lower_divisors.append(i)
-            if i != n // i:
-                upper_divisors.append(n//i)
-        i += 1
-    return lower_divisors + upper_divisors[::-1]
-
-
-L = make_divisors(p)
-ans = 0
-for i in range(len(L)-1):
-    for l in range(len(L)):
-        if L[i]+L[l] == s:
-            ans = 1
-            break
-if ans:
-    print('Yes')
-else:
-    print('No')
+ans = 'No'
+for i in range(10**6+100):
+    if i*(s-i) == p:
+        ans = 'Yes'
+        break
+print(ans)
