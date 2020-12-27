0833dbade09bd3d66c6b39a9e190eecdc9aa3dba
siberian122 <siberian1000@gmail.com>
2020-12-28 00:02:46 +0900

    Create Friend.py

---
+++ practice/Friend.py
@@ -0,0 +1,62 @@
+n, m = map(int, input().split())
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.par = [-1] * (n + 1)
+        self.sizes = [1] * (n + 1)
+
+    # 検索
+    def find(self, x):
+        if self.par[x] < 0:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    # 併合
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        d1 = self.par[x]
+        d2 = self.par[y]
+        if d1 <= d2:
+            self.par[y] = x
+            self.sizes[x] = self.sizes[x] + self.sizes[y]
+            if d1 == d2:
+                self.par[x] = self.par[x] - 1
+        else:
+            self.par[x] = y
+            self.sizes[y] = self.sizes[x] + self.sizes[y]
+
+    # 同じ集合に属するか判定
+    def same_check(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # サイズ
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
+    # 木の数
+    def number(self):
+        count = 0
+        for i in self.par[1:]:
+            if i < 0:
+                count = count + 1
+        return count
+
+
+Tree = UnionFind(n)
+for i in range(m):
+    a, b = map(int, input().split())
+    Tree.union(a, b)
+
+ans = 0
+ans_list = []
+
+for i in range(n):
+    ans_list.append(Tree.size(i))
+
+print(max(ans, max(ans_list)))
