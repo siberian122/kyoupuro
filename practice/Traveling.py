c3e13e372242615c3e303a45080b3df31c94c35a
siberian122 <siberian1000@gmail.com>
2020-12-27 23:23:40 +0900

    Update Traveling.py

--- practice/Traveling.py
+++ practice/Traveling.py
@@ -1,15 +1,10 @@
 n = int(input())
-ans = "No"
 t = 0
 cnt_x = 0
 cnt_y = 0
 for _ in range(n):
     tt, x, y = map(int, input().split())
-    time = tt-t
-    num = abs(cnt_x-x)+abs(cnt_y-y)
-    if num <= t and t % 2 == num % 2:
-        ans = "Yes"
-    else:
-        ans = 'No'
+    if (abs(cnt_x-x)+abs(cnt_y-y)) > abs(tt-t) or (x+y+tt) % 2 != 0:
+        exit(print("No"))
     t, cnt_x, cnt_y = tt, x, y
-print(ans)
+print("Yes")
