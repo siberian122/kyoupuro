ceba1026b8fa8cfd9e3c5abf90e32c74dca055de
siberian122 <siberian1000@gmail.com>
2021-01-05 20:13:14 +0900

    Update Marks.py

--- practice/Marks.py
+++ practice/Marks.py
@@ -1,13 +1,7 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
-past = []
-for i in range(k-1, n):
-    num = 1
-    for l in range(k):
-        num *= a[i-l]
-    past.append(num)
-for i in range(len(past)-1):
-    if past[i] < past[i+1]:
+for i in range(n-k):
+    if a[i] < a[i+k]:
         print("Yes")
     else:
         print("No")
