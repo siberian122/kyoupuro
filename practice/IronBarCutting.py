ef7ee8bb338c53ae929f56d1ea080698e0fa8fbb
siberian122 <siberian1000@gmail.com>
2021-01-07 20:37:13 +0900

    Update IronBarCutting.py

--- practice/IronBarCutting.py
+++ practice/IronBarCutting.py
@@ -2,7 +2,9 @@ n = int(input())
 a = list(map(int, input().split()))
 ans = float('inf')
 l = sum(a)
-
+r = 0
 for i in range(n-1):
-    ans = min(ans, abs(l-2*a[i]))
+    r += a[i]
+    l -= a[i]
+    ans = min(ans, abs(r-l))
 print(ans)
