3e8ccac6299b3c538688b4d71fefa445ae29a67f
siberian122 <siberian1000@gmail.com>
2021-01-07 21:59:18 +0900

    Update TrainTicket.py

--- practice/TrainTicket.py
+++ practice/TrainTicket.py
@@ -1,9 +1,10 @@
 abcd = input()
+# bit全探索
 for i in range(2**3):
     a = int(abcd[0])
     ans = abcd[0]
     for j in range(3):
-        if (i >> j) & 0:
+        if (i >> j) & 1:
             a += int(abcd[j+1])
             ans += "+"+abcd[j+1]
         else:
