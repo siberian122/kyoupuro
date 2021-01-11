c71f24862851b7554ea8d4d3e547de1c86f4d1af
siberian122 <siberian1000@gmail.com>
2021-01-11 19:39:00 +0900

    Create SortedArrays.py

---
+++ practice/SortedArrays.py
@@ -0,0 +1,23 @@
+n = int(input())
+a = list(map(int, input().split()))
+ans = 0
+b = ""
+up, down = 0, 0
+for i in range(n):
+    if b == "" and up == 0 and down == 0:
+        b = a[i]
+    else:
+        if a[i] > b:  # 前より大きい時
+            if down:  # 変わり目
+                up, down = 0, 0
+                ans += 1
+            else:  # 単調増加し続ける
+                up = 1
+        else:  # 前より小さい時
+            if up:
+                up, down = 0, 0
+                ans += 1
+            else:
+                down = 1
+        b = a[i]
+print(ans)
