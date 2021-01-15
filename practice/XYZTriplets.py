7dc06ff6284fc28dee7993242ddb1693e4acd9bf
siberian122 <siberian1000@gmail.com>
2021-01-15 18:26:26 +0900

    Create XYZTriplets.py

---
+++ practice/XYZTriplets.py
@@ -0,0 +1,11 @@
+n = int(input())
+f = [0 for _ in range(10050)]
+for i in range(1, 105):
+    for j in range(1, 105):
+        for k in range(1, 105):
+            num = i*i + j*j + k*k + i*j+j*k+k*i
+            if num < 10050:
+                f[num] += 1
+print(f[:n])
+for i in range(n):
+    print(f[i+1])
