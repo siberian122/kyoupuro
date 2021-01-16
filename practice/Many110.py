48ed89fbabce714a7296e0b99e67526fd21f0a71
siberian122 <siberian1000@gmail.com>
2021-01-16 23:07:34 +0900

    Update Many110.py

--- practice/Many110.py
+++ practice/Many110.py
@@ -1,12 +1,16 @@
 n = int(input())
 t = input()
-ans=0
-s="110"*10**6
-
-if s[0:n] == t:
-    ans=10**10-(n-1)//3
-elif s[1:n+1] == t:
-    ans=10**10 -n//3
-elif s[2:n+2] == t:
-    ans=10**10 -(n+1)//3
+ans = 0
+if t == "1":
+    ans = 2*(10**10)
+elif t == "11":
+    ans = 10**10
+else:
+    num = t.count("0")
+    if t[-1] == "0":
+        ans = 10**10-num+1
+    else:
+        ans = 10**10-num
+if t not in "110" * (n//+1):
+    ans = 0
 print(ans)
