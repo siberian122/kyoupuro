43141d0045f5bb898e2ee8589b5f3363eba48ee5
siberian122 <siberian1000@gmail.com>
2021-01-17 21:37:51 +0900

    Update WaterBottle.py

--- practice/WaterBottle.py
+++ practice/WaterBottle.py
@@ -2,7 +2,7 @@ import math
 
 a, b, x = map(int, input().split())
 cap = a*a*b
-if cap/2 < x:
+if cap/2 < x:  # 半分より多い
     print(math.degrees(math.atan(2*b/a - 2*x/a/a/a)))
-else:
+else:  # 半分以下
     print(math.degrees(math.atan(a*b*b/2/x)))
