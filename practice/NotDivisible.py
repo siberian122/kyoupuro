1fbc11b6e1db66cd2405213563f1aaf4db2c9668
siberian122 <siberian1000@gmail.com>
2021-01-19 14:28:38 +0900

    Update NotDivisible.py

--- practice/NotDivisible.py
+++ practice/NotDivisible.py
@@ -1,10 +1,20 @@
 n = int(input())
 a = list(map(int, input().split()))
-cumsum = 1
+m = max(a)
+dp = [0 for _ in range(m+1)]
 for i in a:
-    cumsum *= i
+    if dp[i] != 0:
+        dp[i] = 2
+        continue
+    num = i
+    for _ in range(m):
+        if num > m:
+            break
+        dp[i] += 1
+        num += i
+# print(dp)
 ans = 0
 for i in a:
-    if (cumsum//i) % i >= 1:
+    if dp[i] == 1:
         ans += 1
 print(ans)
