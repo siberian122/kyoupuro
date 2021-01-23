6d5cba1527604b4dd0fe3b3406da2ad3e20180fc
siberian122 <siberian1000@gmail.com>
2021-01-23 23:25:08 +0900

    Update C.py

--- ABC189/C.py
+++ ABC189/C.py
@@ -3,10 +3,7 @@ a = list(map(int, input().split()))
 ans = []
 for i, j in enumerate(a):
     num = i
-    for l in range(i+1, n):
-        if j > a[l]:
-            num = l
-            break
+    num = a.index()
     if num == i:
         num = n
     ans.append(j*(num-i))
