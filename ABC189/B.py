1743b10113297fffb2a1c4c4294a148258a586b6
siberian122 <siberian1000@gmail.com>
2021-01-23 23:42:30 +0900

    Update B.py

--- ABC189/B.py
+++ ABC189/B.py
@@ -2,7 +2,7 @@ n, x = map(int, input().split())
 cnt = 0
 for i in range(n):
     v, p = map(int, input().split())
-    cnt = v*p
+    cnt += v*p
     if cnt > x*100:
         exit(print(i+1))
 print(-1)
