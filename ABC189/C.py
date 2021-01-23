46caa6820aee613e089e2ddf9d6260b9c6816b35
siberian122 <siberian1000@gmail.com>
2021-01-23 23:42:26 +0900

    Update C.py

--- ABC189/C.py
+++ ABC189/C.py
@@ -1,10 +1,9 @@
 n = int(input())
 a = list(map(int, input().split()))
-ans = []
-for i, j in enumerate(a):
-    num = i
-    num = a.index()
-    if num == i:
-        num = n
-    ans.append(j*(num-i))
-print(max(ans))
+ans = 0
+for i in range(n):
+    cnt = a[i]
+    for j in range(i, n):
+        cnt = min(cnt, a[j])
+        ans = max(ans, (cnt*(j-i+1)))
+print(ans)
