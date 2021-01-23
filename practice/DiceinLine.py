48ceb560e0bef6cad0b61a20b76c5c2582334b56
siberian122 <siberian1000@gmail.com>
2021-01-23 14:34:38 +0900

    Update DiceinLine.py

--- practice/DiceinLine.py
+++ practice/DiceinLine.py
@@ -1,11 +1,11 @@
 n, k = map(int, input().split())
 p = list(map(int, input().split()))
 pp = [(1+x)/2 for x in p]
-ruiseki = [pp[0]]
+ruiseki = [0]
 ans = 0
-for i in range(1, n):
-    ruiseki.append(ruiseki[i-1]+pp[i])
-print(len(ruiseki))
-for i in range(n-k):
-    ans = max(ans, ruiseki[i+k]-ruiseki[i])
+for i in range(n):
+    ruiseki.append(ruiseki[i]+pp[i])
+for i in range(n):
+    if i+k <= n:
+        ans = max(ans, ruiseki[i+k]-ruiseki[i])
 print(ans)
