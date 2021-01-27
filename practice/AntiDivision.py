c41b1c652874895c3accd6fa217a6f7dbd9b9926
siberian122 <siberian1000@gmail.com>
2021-01-27 22:25:04 +0900

    Update AntiDivision.py

--- practice/AntiDivision.py
+++ practice/AntiDivision.py
@@ -1,1 +1,15 @@
+import math
 a, b, c, d = map(int, input().split())
+
+
+def count(n, c, d):
+    l = c * d // math.gcd(c, d)
+    nc = n//c
+    nd = n//d
+    ncd = n//l
+    return n-(nc+nd-ncd)
+
+
+cntA = count(a-1, c, d)
+cntB = count(b, c, d)
+print(cntB-cntA)
