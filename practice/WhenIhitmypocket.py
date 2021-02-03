f6be7b684959777d484554782068e3ff3db44262
siberian122 <siberian1000@gmail.com>
2021-02-03 22:46:36 +0900

    Update WhenIhitmypocket.py

--- practice/WhenIhitmypocket.py
+++ practice/WhenIhitmypocket.py
@@ -1,13 +1,15 @@
 k, a, b = map(int, input().split())
-bis, yen = 1, 0
-for i in range(k):
-    if yen > 0:
-        bis += yen*b
-        yen = 0
-    elif a <= bis and k-i > 1:
-        bis -= a
-        yen += 1
-    else:
-        bis += 1
-    #print(bis, yen)
-print(bis)
+if b-a < 3:
+    # 変えても意味ない
+    print(k+1)
+else:
+    # 回数-交換できるまでの枚数(叩く回数)/2➞お金に変えられる回数
+    cnt = (k-(a-1))//2
+    if cnt < 0:
+        cnt = 0
+    if k < a:  # 　叩いても変えられない
+        print(k+1)
+    ans = a+cnt*(b-a)
+    if cnt * 2+a-1 < k:
+        ans += 1
+    print(ans)
