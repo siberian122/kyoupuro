367fe448ab06053e0fea1bc92fb37374d8bde3a9
siberian122 <siberian1000@gmail.com>
2021-02-08 22:14:03 +0900

    Update Biscuits.py

---
+++ practice/Biscuits.py
@@ -0,0 +1,15 @@
+n, p = map(int, input().split())
+a = list(map(int, input().split()))
+even, odd = 0, 0
+for i in a:
+    if i % 2:
+        odd += 1
+    else:
+        even += 1
+if odd == 0:
+    if P == 1:
+        print(0)
+    else:
+        print(2**n)
+else:
+    print(2**(n-1))
