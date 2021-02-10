602bf3bebd526e2929940de98f0928d5fd11eada
siberian122 <siberian1000@gmail.com>
2021-02-10 21:59:00 +0900

    Update Bounding.py

--- practice/Bounding.py
+++ practice/Bounding.py
@@ -1,7 +1,11 @@
 n, x = map(int, input().split())
 l = list(map(int, input().split()))
-ll = [l[i-1]+l[i] for i in range(1, n)]
-for i in range(n):
-    if ll[i] > x:
-        print(i)
+ans = 1
+cnt = 0
+for i in l:
+    cnt += i
+    if cnt <= x:
+        ans += 1
+    else:
         break
+print(ans)
