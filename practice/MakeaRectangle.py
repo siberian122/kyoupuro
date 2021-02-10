c59bce072f08880d157a98e5d2b3384b9f866f6d
siberian122 <siberian1000@gmail.com>
2021-02-10 22:00:31 +0900

    Update MakeaRectangle.py

--- practice/MakeaRectangle.py
+++ practice/MakeaRectangle.py
@@ -6,7 +6,7 @@ l = dict(collections.Counter(a))
 for i, j in l.items():
     if j >= 2:
         ans.append(i)
-    elif j >= 4:
+    if j >= 4:
         ans.append(i)
 ans.sort()
 print(ans[-1]*ans[-2])
