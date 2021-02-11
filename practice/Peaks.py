d71a6323e8b25ac2a4b8d63ac2570c470aceb2c2
siberian122 <siberian1000@gmail.com>
2021-02-11 23:34:21 +0900

    Update Peaks.py

--- practice/Peaks.py
+++ practice/Peaks.py
@@ -1,63 +1,26 @@
 n,m=map(int,input().split())
 h=list(map(int,input().split()))
+d =dict()
 
-class UnionFind:
-    def __init__(self, n):
-        self.par = [-1] * (n + 1)
-        self.sizes = [1] * (n + 1)
-
-    # 検索
-    def find(self, x):
-        if self.par[x] < 0:
-            return x
-        else:
-            self.par[x] = self.find(self.par[x])
-            return self.par[x]
-
-    # 併合
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        if x == y:
-            return
-        d1 = self.par[x]
-        d2 = self.par[y]
-        if d1 <= d2:
-            self.par[y] = x
-            self.sizes[x] = self.sizes[x] + self.sizes[y]
-            if d1 == d2:
-                self.par[x] = self.par[x] - 1
-        else:
-            self.par[x] = y
-            self.sizes[y] = self.sizes[x] + self.sizes[y]
-
-    # 同じ集合に属するか判定
-    def same_check(self, x, y):
-        return self.find(x) == self.find(y)
-
-    # サイズ
-    def size(self, x):
-        return self.sizes[self.find(x)]
-
-    # 木の数
-    def number(self):
-        count = 0
-        for i in self.par[1:]:
-            if i < 0:
-                count = count + 1
-        return count
-
-tree = UnionFind(n)
-for _ in range(m):
-    a,b=map(int,input().split())
-    tree.union(a,b)
-
-ans = 0
-for i in range(n):
-    now = h[i]
-    for j in tree[i]:
-        if now < h[j]:
+for i in range(m):
+    a,b =map(int,input().split())
+    if a not in d:
+        d[a]=[b]
+    else:
+        d[a].append(b)
+    
+    if b not in d:
+        d[b] = [a]
+    else:
+        d[b].append(a)    
+cnt = 0
+for i in d.keys():
+    flag = True
+    for j in d[i]:
+        if  h[i-1] <= h[j-1]:
+            flag = False
             break
-    ans +=1 
-print(ans)
+    if flag:
+        cnt+=1
 
+print(n-len(d)+cnt)
