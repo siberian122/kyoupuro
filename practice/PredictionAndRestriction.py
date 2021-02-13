8060d498e0c97e1665576f1c19ee1cfb844fa41a
siberian122 <siberian1000@gmail.com>
2021-02-13 21:41:42 +0900

    Update PredictionAndRestriction.py

--- practice/PredictionAndRestriction.py
+++ practice/PredictionAndRestriction.py
@@ -1,29 +1,16 @@
 n, k = map(int, input().split())
 r, s, p = map(int, input().split())
+point = {"r": p, "s": r, "p": s}
 t = input()
 ans = 0
-for i in range(k):
-    now = t[i]
-    if now == "r":
-        ans += p
-    elif now == "s":
-        ans += r
+K = ""
+for i in t:
+    if len(K) < k:
+        ans += point[i]
+        K += i
+    elif K[-k] == i:
+        K += "N"  # None
     else:
-        ans += s
-K = t[k-1]
-for i in range(k, n):
-    now = t[i]
-
-    if now == "r" and K != "r":
-        ans += p
-        K = now
-    elif now == "s" and K != "s":
-        ans += r
-        K = now
-    elif now == "p" and K != "p":
-        ans += s
-        K = now
-    else:
-        # k回前と同じなら何出してもいい
-        K = "lose"
+        ans += point[i]
+        K += i
 print(ans)
