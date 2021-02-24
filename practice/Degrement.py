f97fef13c2c3307993706ba2afc700a1c79d10bb
siberian122 <siberian1000@gmail.com>
2021-02-24 19:32:07 +0900

    Update Degrement.py

--- practice/Degrement.py
+++ practice/Degrement.py
@@ -1,15 +1,7 @@
 n = int(input())
 p = list(map(int, input().split()))
 ans = 0
-while True:
-    cnt = 0
-    for i in range(n-1):
-        if i+1 == p[i]:
-            p[i], p[i+1] = p[i+1], p[i]
-            ans += 1
-            continue
-        else:
-            cnt += 1
-    if cnt == n-1:
-        break
+for i in range(n):
+    if p[i] == i+1:
+        ans += 1
 print(ans)
