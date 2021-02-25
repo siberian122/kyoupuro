8107e4f6c8c5dc46312834ce079e3beccf410454
siberian122 <siberian1000@gmail.com>
2021-02-25 21:16:41 +0900

    Update MonstersBattleRoyale.py

--- practice/MonstersBattleRoyale.py
+++ practice/MonstersBattleRoyale.py
@@ -1,8 +1,8 @@
+import math
 n = int(input())
 a = sorted(list(map(int, input().split())))
 ans = a[0]
 for i in range(1, n):
-    cnt = sum(a[:i])
-    if cnt < a[i]:
-        ans = min(a[i]-cnt, ans)
+    A = a[i]
+    ans = math.gcd(A.gcd)
 print(ans)
