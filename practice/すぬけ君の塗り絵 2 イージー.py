03e3cf0216e86e44d4142738e7a15c484a3e05d6
siberian122 <siberian1000@gmail.com>
2021-02-26 16:26:52 +0900

    Create すぬけ君の塗り絵 2 イージー.py

---
+++ practice/すぬけ君の塗り絵 2 イージー.py
@@ -0,0 +1,15 @@
+w, h, n = map(int, input().split())
+yoko = 0
+tate = 0
+for _ in range(n):
+    x, y, a = map(int, input().split())
+    if a == 1:
+        yoko = max(yoko, x)
+    elif a == 2:
+        w = min(w, x)
+    elif a == 3:
+        tate = max(tate, y)
+    elif a == 4:
+        h = min(h, y)
+# print(h, tate, w, yoko)
+print(max(w-yoko, 0)*max(y-tate, 0))
