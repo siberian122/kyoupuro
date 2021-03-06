c1500af4c90ae5c793b13e2cc307406cf74432ab
siberian122 <siberian1000@gmail.com>
2021-03-06 23:28:25 +0900

    Create B.py

---
+++ ABC194/B.py
@@ -0,0 +1,26 @@
+n = int(input())
+person = []
+for i in range(n):
+    a, b = map(int, input().split())
+    person.append([a, b, i])
+wa = min([x+y for x, y, z in person])
+
+cnt_a = sorted(person, key=lambda x: x[0])
+cnt_b = sorted(person, key=lambda x: x[1])
+a = cnt_a[1][0]
+b = cnt_b[1][1]
+
+"""
+print(cnt_a)
+print(cnt_b)
+print(cnt_a[0][2])
+"""
+if cnt_a[0][2] == cnt_b[0][2]:
+    if cnt_a[0][0] != cnt_b[0][1]:
+        cnt = wa
+    else:
+        cnt = min(a, b)
+    print(min(wa, cnt))
+
+else:
+    print(max(cnt_a[0][0], cnt_b[0][1]))
