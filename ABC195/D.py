a44bb6c6f0c9f5d6425cf424d99299d06ec3448c
siberian122 <siberian1000@gmail.com>
2021-03-13 22:50:20 +0900

    Create D.py

---
+++ ABC195/D.py
@@ -0,0 +1,19 @@
+n, m, q = map(int, input().split())
+nimotu = [list(map(int, input().split())) for _ in range(n)]
+nimotu = sorted(nimotu, reverse=True, key=lambda x: x[1])
+# print(nimotu)
+x = list(map(int, input().split()))
+for _ in range(q):
+    ans = 0
+    l, r = map(int, input().split())
+    print(x[:l-1]+x[r:])
+    box = sorted(x[:l-1]+x[r:])
+    ined = []
+    print(box)
+    for j in box:
+        for i in range(n):
+            if j >= nimotu[i][0] and i not in ined:
+                ans += nimotu[i][1]
+                ined.append(i)
+                break
+    print(ans)
