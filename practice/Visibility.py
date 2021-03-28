153bd85f1e38df450b94fb98c90dbdcabe7491ae
siberian122 <siberian1000@gmail.com>
2021-03-28 21:17:08 +0900

    Create Visibility.py

---
+++ practice/Visibility.py
@@ -0,0 +1,26 @@
+h, w, x, y = map(int, input().split())
+s = [input() for _ in range(h)]
+ans = 0
+x -= 1
+y -= 1
+for i in range(x, w):
+    if s[i][y] == ".":
+        ans += 1
+    else:
+        break
+for i in reversed(range(0, x)):
+    if s[i][y] == ".":
+        ans += 1
+    else:
+        break
+for i in range(y, w):
+    if s[x][i] == ".":
+        ans += 1
+    else:
+        break
+for i in reversed(range(0, y)):
+    if s[x][i] == ".":
+        ans += 1
+    else:
+        break
+print(ans - 1)
